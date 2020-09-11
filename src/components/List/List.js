1ec67e38324355cea0336f671ec0cad6c4c9b436
Baird Lackner-Buckingham <bbuckingham@acumium.com>
2020-09-10 22:59:50 -0500

    Starts branding for WSMA

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,9 +2,23 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import './List.scss'
+import ListItem from './ListItem'
 
-const List = ({className=''}) => {
-  return <div>List</div>
+const List = ({ className = '', items = [] }) => {
+  return (
+    <section className="ds-list">
+      {items.map((item) => {
+        return (
+          <ListItem
+            title={item.title}
+            subtitle={item.subtitle}
+            description={item.description}
+            key={item.id}
+          />
+        )
+      })}
+    </section>
+  )
 }
 
 List.propTypes = {}
