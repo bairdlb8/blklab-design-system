74220ccce86ece758f95282926d6c5bf1de1b465
Baird Lackner-Buckingham <bbuckingham@acumium.com>
2020-09-13 22:30:48 -0500

    Adds value prop to TextInput

--- src/basics/TextInput/TextInput.js
+++ src/basics/TextInput/TextInput.js
@@ -7,82 +7,89 @@ const emailReg = new RegExp(
   /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 )
 
-const TextInput = forwardRef(({
-  label,
-  type = 'text',
-  inputMode = 'text',
-  error = false,
-  errorMessage = '',
-  onValidate = () => {},
-  required,
-  name,
-  helpText,
-  disabled,
-  placeholder,
-  fullWidth = false,
-  onChange = null,
-  icon,
-  iconSide = 'left',
-  clear
-}, ref) => {
-  const Messages = {
-    empty: 'Please fill out this field',
-    wrong: errorMessage
-  }
+const TextInput = forwardRef(
+  (
+    {
+      label,
+      type = 'text',
+      inputMode = 'text',
+      error = false,
+      errorMessage = '',
+      onValidate = () => {},
+      required,
+      name,
+      helpText,
+      disabled,
+      placeholder,
+      fullWidth = false,
+      onChange = null,
+      icon,
+      iconSide = 'left',
+      clear,
+      value
+    },
+    ref
+  ) => {
+    const Messages = {
+      empty: 'Please fill out this field',
+      wrong: errorMessage
+    }
 
-  const [state, setState] = useState({
-    visible: false,
-    error: error,
-    errorState: 'wrong'
-  })
+    const [state, setState] = useState({
+      visible: false,
+      error: error,
+      errorState: 'wrong'
+    })
 
-  const validate = (ev) => {
-    if (required === true && !ev.target.value.trim()) {
-      setState({ ...state, error: true, errorState: 'empty' })
-    } else {
-      if (type == 'email' && !emailReg.test(ev.target.value.trim())) {
-        setState({ ...state, error: true, errorState: 'wrong' })
+    const validate = (ev) => {
+      if (required === true && !ev.target.value.trim()) {
+        setState({ ...state, error: true, errorState: 'empty' })
       } else {
-        setState({ ...state, error: false, errorState: 'wrong' })
+        if (type == 'email' && !emailReg.test(ev.target.value.trim())) {
+          setState({ ...state, error: true, errorState: 'wrong' })
+        } else {
+          setState({ ...state, error: false, errorState: 'wrong' })
+        }
       }
     }
-  }
 
-  return (
-    <div className="ds-input">
-      <label>{label}</label>
-      {helpText && <div className="help">{helpText}</div>}
-      <div
-        className={`ds-input__input ${state.error ? 'has-error' : null} ${
-          fullWidth ? 'full-width' : null
-        }`}
-      >
-        {icon && iconSide === 'left' && <Icon icon={icon} size="large" />}
-        <input
-          ref={ref}
-          name={name}
-          type={type === 'password' && state.visible ? 'text' : type}
-          inputMode={inputMode}
-          disabled={disabled}
-          placeholder={placeholder}
-          className={`display-block`}
-          disabled={disabled ? true : null}
-          onChange={onChange}
-          required={required}
-          onBlur={validate}
-        />
-        {icon && iconSide === 'right' && <Icon icon={icon} />}
-        {clear && <Icon size="large" icon="close-circle-outline" />}
-        {type == 'password' && (
-          <button onClick={() => setState({ ...state, visible: !state.visible })}>
-            {!state.visible ? 'Show' : 'Hide'}
-          </button>
-        )}
+    return (
+      <div className="ds-input">
+        <label>{label}</label>
+        {helpText && <div className="help">{helpText}</div>}
+        <div
+          className={`ds-input__input ${state.error ? 'has-error' : null} ${
+            fullWidth ? 'full-width' : null
+          }`}
+        >
+          {icon && iconSide === 'left' && <Icon icon={icon} size="large" />}
+          <input
+            ref={ref}
+            name={name}
+            type={type === 'password' && state.visible ? 'text' : type}
+            inputMode={inputMode}
+            disabled={disabled}
+            placeholder={placeholder}
+            className={`display-block`}
+            disabled={disabled ? true : null}
+            onChange={onChange}
+            required={required}
+            onBlur={validate}
+            value={value}
+          />
+          {icon && iconSide === 'right' && <Icon icon={icon} />}
+          {clear && <Icon size="large" icon="close-circle-outline" />}
+          {type == 'password' && (
+            <button onClick={() => setState({ ...state, visible: !state.visible })}>
+              {!state.visible ? 'Show' : 'Hide'}
+            </button>
+          )}
+        </div>
+        <div className="error">{state.error ? Messages[state.errorState] : ''}</div>
       </div>
-      <div className="error">{state.error ? Messages[state.errorState] : ''}</div>
-    </div>
-  )
-})
+    )
+  }
+)
 
 export default TextInput
 
