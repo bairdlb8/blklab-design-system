7914a810379988fabbc43cb7e7544b4dffe84ea9
Baird Lackner-Buckingham <bbuckingham@acumium.com>
2020-09-15 12:41:39 -0500

    Gives snackbar an animation on load

--- src/components/Snackbar/Snackbar.js
+++ src/components/Snackbar/Snackbar.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 
 import './Snackbar.scss'
 
 const Snackbar = ({ className = '', children, status = 'idle', onDismiss }) => {
+  const [state, setState] = useState({
+    uiState: 'hidden'
+  })
+
+  useEffect(() => {
+    setTimeout(() => {
+      setState({ ...state, uiState: 'show' })
+    }, 100)
+  }, [])
+
   return (
-    <div className={`ds-snackbar ${status}`}>
+    <div className={`ds-snackbar ${state.uiState} ${status}`}>
       <div className="ds-snackbar__message">{children}</div>
       {onDismiss && (
         <div className="ds-snackbar__action" onClick={onDismiss}>
