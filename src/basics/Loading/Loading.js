d418bdb64892e7469130555a6851a9272d2ad200
Baird Lackner-Buckingham <bbuckingham@acumium.com>
2020-10-09 12:56:05 -0500

    Apply modifications from app

--- src/basics/Loading/Loading.js
+++ src/basics/Loading/Loading.js
@@ -4,14 +4,7 @@ import PropTypes from 'prop-types'
 import './Loading.scss'
 
 const Loading = ({ className = '' }) => {
-  return (
-    <div className="loading">
-      <div className="double">
-        <div className="double-bounce1"></div>
-        <div className="double-bounce2"></div>
-      </div>
-    </div>
-  )
+  return <div className="ds-loading"></div>
 }
 
 Loading.propTypes = {}
